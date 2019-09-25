2c754f468c872354710bc8c5c0e8a5bc4e38f1d9
Matt-Y-26 <my286@njit.edu>
2019-09-25 11:23:31 -0400

    Adding sql folder with files and also redoing the initDB and having a new one

--- initDB.php
+++ initDB.php
@@ -1,39 +1,36 @@
-<?php #turn error reporting on ini_set('display_errors',1);; ini_set('display_startup_errors', 1); 
+<?php
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+//pull in config.php so we can access the variables from it
 require('config.php');
-echo $host;
-
- 
-$connection_string = "mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
-
+//echo "Loaded Host: " . $host;
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-	$db = new PDO($connection_string, $username, $password);
-	echo "Should have connected";
-
-	$query ="create table if not exists `TestUsers` (
-		`id` int auto_increment not null,
-		`username`  varchar(30) not null unique,
-		`pin` int default 0,
-		PRIMARY KEY (`id`)
-		) CHARACTER SET utf8 COLLATE utf8_general_ci";
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-	$stmt = $db->prepare($query);
-	print_r($stmt->errorInfo());
-	$r = $stmt->execute();
-	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
-	
-	$insert_query = "INSERT INTO `TestUsers` ( `username`, `pin`) VALUES ('JohnDoe', 1234)";
-	$stmt = $db->prepare($insert_query);
-	$r = $stmt->execute();
-
-	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
-
+	foreach(glob("sql/*.sql") as $filename){
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[$filename];
+	}
+	ksort($sql);
+	//connect to DB
+	$db = new PDO($conn_string, $username, $password);
+	//$db->setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value){
+		echo "<br>Running: " . $key;
+		$stmt = $db->prepare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		if($error && $error[0] !== '00000'){
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+		echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+	}
 	
 }
 catch(Exception $e){
 	echo $e->getMessage();
-	exit("It didn't work");
+	exit("It didnt work");
 }
 ?>
-
