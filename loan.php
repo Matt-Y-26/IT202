e210866a73b2f751254510aafd7061c9d1d41ebc
Matt-Y-26 <my286@njit.edu>
2019-12-11 12:48:58 -0500

    Fix stuff

--- loan.php
+++ loan.php
@@ -25,33 +25,37 @@ you have to pay an additional 5% of what was taken out </p>
 	<input type="submit" value="Takeout Loan" name="button">
 	<input type="submit" value="Repay Loan" name="button">
 </form>
+<a href="landingpg.php">Back to dashboard</a>
 </html>
 
 <?php
 	if(isset($_POST["button"]))
 	{
 		require("config.php");
-		//$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-		//$db = new PDO($conn_string, $username, $password);
+		$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+		$db = new PDO($conn_string, $username, $password);
 			
 		$idnum=$_SESSION['id'];
 		if($_POST["button"]=="Takeout Loan")
 		{//takeout loan, require loanamt
 		
-			if(isset($_POST["loanamt"))
+			if(isset($_POST["loanamt"]))
 			{
 				//add money to loan session AND db, add to loanamt AND db
 				$_SESSION['loanmoney']+=$_POST["loanamt"];
 				$_SESSION['loanamt']+=$_POST["loanamt"];
+				echo "You now have " . $_SESSION["loanmoney"] . " in loan money";
+				echo "<br></br>";
+				echo "You now have " . $_SESSION["loanamt"] . " in loan debt/taken";
 				//update db
 		
-	//$stmt = $db->prepare("UPDATE `ProjUsers` SET `loan` =:loan `loanamt`=:loanamt WHERE ID =:id");
-		//$result = $smt->execute(
-		//array(":loan"=>$_SESSION['loanmoney'],
-		//	":loanamt"=>$SESSION['loanamt'],
-		//	":id"=>$idnum
-		//	)
-		//);
+	$stmt = $db->prepare("UPDATE `ProjUsers` SET `loanmoney` =:loan, `loanamt`=:loanamt WHERE ID =:id");
+		$result = $stmt->execute(
+		array(":loanmoney"=>$_SESSION['loanmoney'],
+			":loanamt"=>$_SESSION['loanamt'],
+			":id"=>$idnum
+			)
+		);
 			
 		}
 		
